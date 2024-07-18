761e90110f123ae26db8a64ea7c04e0ee3f5c000
MediateFrystal <mediatefrystal@qq.com>
2024-07-18 19:19:15 +0800

    Add Lightbox

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'MediateFrystal',
-  subtitle: 'Demo Site',
+  subtitle: 'Test Site',
   lang: 'Zh_CN', // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -36,7 +36,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari', // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/MediateFrystal/crispy-barnacle', // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
     },
   ],
@@ -44,15 +44,15 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
+  name: 'MediateFrystal',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter', // Visit https://icones.js.org/ for icon codes
+      name: 'X',
+      icon: 'fa6-brands:x-twitter', // Visit https://icones.js.org/ for icon codes
       // You will need to install the corresponding icon set if it's not already included
       // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'https://x.com',
     },
     {
       name: 'Steam',
